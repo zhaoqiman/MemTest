e9e00be3b36151c4bdcf2f209f755d02f7ac58e0
赵启满 <zhaoqiman030507@gmail.com>
2024-03-27 16:50:33 +0800

    多线程测试crc32.cpp编写完成，x86_64下初步编译测试通过

---
+++ CRC_ReliCheck/crc32.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <ctime>
+#include <random>
+
+// 数据长度/字节
+#define WIDTH 32
+// 总测试次数
+const unsigned int TESTTIME = 1e7;
+
+int crc_init = 0;
+unsigned int crc_table[256];
+std::mutex mtx;
+
+unsigned long collisionCount= 0;
+unsigned long crcCount= 0;
+
+// 初始化 CRC32 校验表
+void crc32gentab(void) {
+    unsigned long crc, poly;
+    int i, j;
+
+    poly = 0xEDB88320L;
+    for (i = 0; i < 256; i++) {
+        crc = i;
+        for (j = 8; j > 0; j--) {
+            if (crc & 1) {
+                crc = (crc >> 1) ^ poly;
+            }
+            else {
+                crc >>= 1;
+            }
+        }
+        crc_table[i] = crc;
+    }
+}
+
+//  计算校验值
+unsigned int crc32(unsigned char* buf, unsigned int len) {
+    unsigned int crc;
+
+    if (crc_init == 0) {
+        crc32gentab();
+        crc_init = 1;
+    }
+
+    crc = 0xffffffff;
+    while (len--) {
+        crc = crc_table[(crc ^ (unsigned char)*buf++) & 0xff] ^ (crc >> 8);
+    }
+    return crc ^ 0xffffffff;
+}
+
+//  计算汉明距离
+int hammingDist(unsigned int a, unsigned int b) {
+    int distance = 0;
+    unsigned int xor_result = a ^ b;
+    while (xor_result) {
+        distance += xor_result & 1;
+        xor_result >>= 1;
+    }
+    return distance;
+}
+
+
+
+// 线程执行totalTime次测试函数
+void oneTest(unsigned long& collisionCount, unsigned long& crcCount, int id, int totalTime) {
+    unsigned char x[WIDTH];
+    unsigned char y[WIDTH];
+
+    int i = 0, j = 0;
+    srand((unsigned)time(0) + id<<8); // 每个线程都要随机
+
+    while (totalTime > 0) {
+        // x 随机
+        for (i = 0; i != WIDTH; i++) {
+            x[i] = dis(gen);
+            y[i] = x[i];
+        }
+
+        unsigned int Cx = crc32(&x[0], WIDTH);
+        unsigned int Cy = 0;
+
+        int dist = 0;
+        // 原 x 逐 bit翻转，即错码 y
+        for (i = 0; i != WIDTH; i++) {      // each Byte of x
+            for (j = 0; j != 8; j++) {      // each bit of each Byte
+                y[i] ^= 1 << j; // 第i字节 第j比特 反转
+                Cy = crc32(&y[0], WIDTH);
+                dist = hammingDist(Cx, Cy);
+
+                switch (dist) {
+                case 0:
+                    mtx.lock();
+                    collisionCount++;
+                    mtx.unlock();
+                    break;
+                case 1:
+                    mtx.lock();
+                    crcCount++;
+                    mtx.unlock();
+                    break;
+                default:
+                    break;
+                }
+                //再反转 恢复 y 原样
+                y[i] ^= 1 << j;
+            }
+        }
+        totalTime--;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int numThreads = std::thread::hardware_concurrency(); // 获取可用的线程数量
+
+    unsigned int totalTime = TESTTIME;
+    //unsigned long collisionCount = 0, crcCount = 0;
+
+    //  创建numThreads个线程向量，每个线程执行 totalTime / numThreads 次
+    std::vector<std::thread> threads;
+    for (int i = 0; i < numThreads; ++i) {
+        threads.push_back(std::thread(oneTest, std::ref(collisionCount), std::ref(crcCount), i, totalTime / numThreads));
+    }
+
+    // 等待所有线程完成
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    unsigned long long allNum = totalTime * WIDTH * 8;
+    std::cout << "Total tested code num is " << totalTime << std::endl;
+    std::cout << "Total possible wrong code num is" << allNum << std::endl;
+    std::cout << "In which, collisionCount is " << collisionCount << std::endl;
+    std::cout << "crcCount is " << crcCount << std::endl;
+    system("pause");
+    return 0;
+}
